4d05e60c797ee549d9a742ae477a6239abf6db0d
dzz <devon.zachary@gmail.com>
2014-03-03 04:05:39 -0800

    ui script updates

--- ui_gen/ui_gen.py
+++ ui_gen/ui_gen.py
@@ -34,3 +34,17 @@ for mapper_name in mappers:
 	command = command.replace("{$slider_idx}","%d" % idx);
 	os.system(command);
 	idx += 1;
+
+buttons = [ "eyedrop","erase", "smudge" ];
+make_button_command = "convert -background transparent -fill {$color} -font Consolas -pointsize 11 {$boldmode} label:\"{$button_name}\" label_{$button_name}.bmp & composite -gravity Center label_{$button_name}.bmp toolbar_{$mode}.bmp toolbar_{$button_idx}_{$mode}.bmp"
+
+idx = 0;
+for button_name in buttons:
+	command = make_button_command.replace("{$button_name}",button_name);
+	command = command.replace("{$button_idx}","%d" % idx);
+
+	up_command = command.replace("{$mode}","up").replace("{$color}","black").replace("{$boldmode}","");
+	down_command = command.replace("{$mode}","down").replace("{$color}","black").replace("$boldmode}","-weight bold");
+	os.system(up_command);
+	os.system(down_command);
+	idx += 1;
