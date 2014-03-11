99752456dc3b5533dfc007100473f37f59552a52
dzz <devon.zachary@gmail.com>
2014-03-11 05:52:23 +0100

    asrgasfd

--- ui_gen/ui_gen.py
+++ ui_gen/ui_gen.py
@@ -1,7 +1,8 @@
+#this is a horrific embarassement - but it works.
 import os
 
 sliders = [ "size", "dab shape", "alpha", "~ nothing ~", "jitter", "noise" ]
-make_slider_command = "convert -background black -fill white -font Consolas -pointsize 12 label:\" {$slider_name} \" -rotate -90 label_{$slider_name}.bmp & composite -gravity Center label_{$slider_name}.bmp be_slider.bmp slider_{$slider_idx}.bmp"
+make_slider_command = "convert -background black -fill white -font Consolas -pointsize 12 label:\" {$slider_name} \" -rotate -90 label_{$slider_name}.bmp & composite -gravity Center label_{$slider_name}.bmp templates/be_slider.bmp generated/slider_{$slider_idx}.bmp"
 
 idx = 0;
 for slider_name in sliders:
@@ -11,7 +12,7 @@ for slider_name in sliders:
 	idx += 1;
 
 buttons = [ "grad","erase", "std","bright","dark" ];
-make_button_command = "convert -background transparent -fill {$color} -font Consolas -pointsize 10 label:\"{$button_name}\" label_{$button_name}.bmp & composite -gravity Center label_{$button_name}.bmp button_{$mode}.bmp button_{$button_idx}_{$mode}.bmp"
+make_button_command = "convert -background transparent -fill {$color} -font Consolas -pointsize 10 label:\"{$button_name}\" label_{$button_name}.bmp & composite -gravity Center label_{$button_name}.bmp templates/button_{$mode}.bmp generated/button_{$button_idx}_{$mode}.bmp"
 
 idx = 0;
 for button_name in buttons:
@@ -26,7 +27,7 @@ for button_name in buttons:
 
 
 mappers = [ "colormix", "alpha", "size", "jitter", "noise" ]
-make_mapper_command = "convert -background transparent -fill green -font Consolas -pointsize 11 label:\" {$mapper_name} \" -rotate 45 label_{$mapper_name}.bmp & composite -gravity Center label_{$mapper_name}.bmp mapper.bmp mapper_{$slider_idx}.bmp"
+make_mapper_command = "convert -background transparent -fill green -font Consolas -pointsize 11 label:\" {$mapper_name} \" -rotate 45 label_{$mapper_name}.bmp & composite -gravity Center label_{$mapper_name}.bmp templates/mapper.bmp generated/mapper_{$slider_idx}.bmp"
 
 idx = 0;
 for mapper_name in mappers:
@@ -36,7 +37,7 @@ for mapper_name in mappers:
 	idx += 1;
 
 buttons = [ "brush","erase", "smudge","eyedrop" ];
-make_button_command = "convert -background transparent -fill {$color} -font Consolas {$boldmode} -pointsize 11 label:\"{$button_name}\" label_{$button_name}.bmp & composite -gravity Center label_{$button_name}.bmp toolbar_{$mode}.bmp toolbar_{$button_idx}_{$mode}.bmp"
+make_button_command = "convert -background transparent -fill {$color} -font Consolas {$boldmode} -pointsize 11 label:\"{$button_name}\" label_{$button_name}.bmp & composite -gravity Center label_{$button_name}.bmp templates/toolbar_{$mode}.bmp generated/toolbar_{$button_idx}_{$mode}.bmp"
 
 idx = 0;
 for button_name in buttons:
@@ -50,15 +51,18 @@ for button_name in buttons:
 	idx += 1;
 
 titles = [ "stylus","brush_controller","mapper" ]
-make_title_command = "convert -background rgb(72,72,72) -fill white -font Consolas -size 100x16 label:\" {$title_name} \" title_{$title_name}.bmp"
+make_title_command = "convert -background rgb(72,72,72) -fill white -font Consolas -size 100x16 label:\" {$title_name} \" generated/title_{$title_name}.bmp"
 
 for title_name in titles:
 		command = make_title_command.replace("{$title_name}",title_name);
 		os.system(command);
 
 titles = [ "pressure","time","size","color","alpha","jitter","noise","input","output" ]
-make_title_command = "convert -background rgb(64,64,64) -fill rgba(165,164,162) -font Consolas -size 60x16  label:\" {$title_name} \" title_{$title_name}.bmp"
+make_title_command = "convert -background rgb(64,64,64) -fill rgba(165,164,162) -font Consolas -size 60x16  label:\" {$title_name} \" generated/title_{$title_name}.bmp"
 
 for title_name in titles:
 		command = make_title_command.replace("{$title_name}",title_name);
 		os.system(command);
+
+os.system("copy templates\\*.bmp generated\\");
+os.system("del *.bmp");
