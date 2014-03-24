c6059ba9f0a1968addfa8f8775c83142dacd54f3
dzz <devon.zachary@gmail.com>
2014-03-24 03:52:23 -0700

    shader tweaks

--- shaders/hw_context.frag.glsl
+++ shaders/hw_context.frag.glsl
@@ -2,15 +2,9 @@
 
 uniform sampler2D ctxt_sampler;
 out vec4 color;
+in vec2 uv;
  
 void main(void) {
-    vec2 sp;
-    ivec2 scrp;
-
-    scrp.x = int(gl_FragCoord.x);
-    scrp.y = int(1080-gl_FragCoord.y);
-
-    color = texelFetch(ctxt_sampler,scrp,0);
-
+    color = texture(ctxt_sampler,uv);
 }
 
