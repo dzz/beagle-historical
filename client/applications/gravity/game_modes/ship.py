f873fd567508e458fc49d7478063d6e8385a34dc
dzz <devon.zachary@gmail.com>
2015-09-13 17:39:09 -0700

    magic

--- client/applications/gravity/game_modes/ship.py
+++ client/applications/gravity/game_modes/ship.py
@@ -4,28 +4,115 @@ from client.gfx.primitive import primitive
 from client.gfx.blend            import blendstate,blendmode
 from client.ctt2.animation import curve_sequencer
 from client.gfx.framebuffer import *
+from client.system.gamepad       import get_gamepad
 
+
+class ow_terminal:
+    def __init__(self,view, ow_player,modBuffer):
+        assets.load_package("sylab")
+        self.modBuffer = modBuffer
+        self.ow_player = ow_player
+        self.view = view
+        self.textures = {}
+        self.textures["sylab_head"] = assets.get("sylab/texture/sylab_phase_0_head")
+        self.primitive = primitive.get_unit_uv_primitive()
+        self.shader = assets.get("sylab/shader/terminal")
+        self.sequencer = curve_sequencer( assets.get("sylab/curve_sequence/disp_warp") )
+        self.parallax = 0.2
+        self.t = 0.0
+        self.x = -0.45
+
+    def get_head_shader_param(self):
+        return {
+            "texBuffer"            : self.textures["sylab_head"],
+            "modBuffer"            : self.modBuffer,
+            "translation_local"    : [self.x-(self.ow_player.x*self.parallax),-0.2],
+            "scale_local"          : self.sequencer.animated_value("terminal_scale"),
+            "translation_world"    : [0.0,0.0],
+            "scale_world"          : [1.0,1.0],
+            "view"                 : self.view,
+            "rotation_local"       : 0.0 ,
+            "warp"                 : self.sequencer.animated_value("warp")[0],
+            "term_cam"             : [self.x-(self.ow_player.x*self.parallax),0.0],
+            "time"                 : self.t,
+            "filter_color"         : self.sequencer.animated_value("filter_color"),
+            "uv_translate"         : [0,0] }
+
+    def render(self):
+        self.primitive.render_shaded(self.shader, self.get_head_shader_param() )
+
+    def tick(self):
+        self.t += 0.1
+        self.sequencer.tick()
+
+class ow_enviro:
+    def __init__(self,config ):
+        self.x_range = [config["room_range"]["left"],config["room_range"]["right"]]
+        self.children = []
+
+    def register_child(self,child):
+        self.children.append(child)
+
+    def handle_wrap_around(self,child):
+        if child.x < self.x_range[0]:
+            child.x = self.x_range[1]
+        if child.x > self.x_range[1]:
+            child.x = self.x_range[0]
+
+    def tick(self):
+        for child in self.children:
+            self.handle_wrap_around(child)
 
 class ow_player:
-    def __init__(self, view):
+    def __init__(self, view ):
         self.walk_sequencer = curve_sequencer( assets.get("overworld_player/curve_sequence/walk_left") )
+        self.intro_sequencer = curve_sequencer( assets.get("overworld_player/curve_sequence/intro_float") )
         self.x = 0
         self.y = 0
         self.primitive = primitive.get_unit_uv_primitive()
         self.view = view
         self.shader = assets.get("common/shader/default_2d")
+        self.walk_sequencer.tick() 
+        self.mirror_walk = True
+        self._is_walking = False
+
+    def get_mirror_scale(self):
+        if( self.mirror_walk):
+            return [ -1.0,1.0 ]
+        else:
+            return [1.0, 1.0]
 
     def tick(self):
-        self.walk_sequencer.tick()
+        if not self.intro_sequencer.is_finished():
+            self.intro_sequencer.tick()
+        else:
+            self.handle_input()
+            if self.is_walking():
+                self.walk_sequencer.tick()
+
+    def handle_input(self):
+        gp = get_gamepad(0)
+        if(gp.leftStick[0]>0.25):
+            self.x += 0.1*gp.leftStick[0]
+            self._is_walking = True
+            self.mirror_walk = True
+        elif(gp.leftStick[0]<-0.25):
+            self.x += 0.1*gp.leftStick[0]
+            self._is_walking = True
+            self.mirror_walk = False
+        else:
+            self._is_walking = False
+    
+    def is_walking(self):
+        return self._is_walking
 
     def get_shader_params(self):
         return {
             "texBuffer"            : assets.get( self.walk_sequencer.animated_value("texture_asset") ),
-            "scale_local"          : [1.0,1.0],
-            "translation_local"    : [0,0,0],
-            "scale_local"          : [1.2,1.5],
-            "translation_world"    : [self.x,self.y],
-            "scale_world"          : [1.0,1.0],
+            "translation_local"    : [0.0,0.0],
+            "scale_local"          : [1.2/2,1.5/2],
+            "translation_world"    : [0,self.y + self.intro_sequencer.animated_value("float_down")[1] ],
+            "scale_world"          : self.get_mirror_scale(),
             "view"                 : self.view,
             "rotation_local"       : 0.0 ,
             "filter_color"         : [1.0,1.0,1.0,1.0],
@@ -64,7 +151,10 @@ class chamber:
 
 class ship_game:
     def __init__(self):
-        assets.load_packages(["station","intro"])
+        assets.load_package("intro")
+        assets.load_package("station")
+        assets.load_package("sylab")
+        self.ow_enviro = ow_enviro( assets.get("sylab/dict/landing_enviro_config") )
         self.star_textures = [ assets.get("station/texture/stars_0"),
                                assets.get("station/texture/stars_1"),
                                assets.get("station/texture/stars_2")]
@@ -79,14 +169,17 @@ class ship_game:
         self.primitive = primitive.get_unit_uv_primitive()
         self.t_delta = 1.0/240.0;
 
-        self.comp_buffer = framebuffer.from_dims(256,128,False)
+        self.comp_buffer = framebuffer.from_screen()
+        self.ow_terminal = ow_terminal(self.view, self.ow_player, self.comp_buffer.get_texture() )
 
         self.t = 0
         self.sequence   = assets.get("station/curve_sequence/station")
         self.sequencer = curve_sequencer( self.sequence )
         self.chamber = chamber(self.primitive,self.view)
 
-        self.sequencer.register_slave( self.ow_player )
+        self.ow_enviro.register_child( self.ow_player )
+        self.sequencer.register_slaves([ self.ow_player, self.ow_terminal, self.ow_enviro])
+        self.sequencer.tick()
 
     def tick(self,context):
         self.t+=self.t_delta
@@ -127,19 +220,19 @@ class ship_game:
         with blendstate(blendmode.add):
             self.primitive.render_shaded( self.star_shader, 
                         { "texBuffer"    : self.star_textures[0],
-                          "uv_translate" : [self.t*0.01,0.0],
+                          "uv_translate" : [self.t*0.01+self.ow_player.x*0.01,0.0],
                           "uv_scale"     : [1,1],
                           "t"            : self.t,
                           "filterColor"  : [0.97,0.97,1.0,1.0]} )
             self.primitive.render_shaded( self.star_shader, 
                         { "texBuffer"    : self.star_textures[1],
-                          "uv_translate" : [self.t*0.05,0.0],
+                          "uv_translate" : [self.t*0.05+self.ow_player.x*0.02,0.0],
                           "uv_scale"     : [0.5,0.5],
                           "t"            : self.t,
                           "filterColor"  : [0.7,0.65,0.65,0.65]} )
             self.primitive.render_shaded( self.star_shader, 
                         { "texBuffer"    : self.star_textures[2],
-                          "uv_translate" : [self.t*0.5,0.0],
+                          "uv_translate" : [self.t*0.5+self.ow_player.x*0.03,0.0],
                           "uv_scale"     : [0.2,0.2],
                           "t"            : self.t,
                           "filterColor"  : [0.28,0.35,0.25,0.25]} )
@@ -148,11 +241,15 @@ class ship_game:
     def render(self,context):
         with render_target(self.comp_buffer):
             self.render_starscroll()
+            with blendstate(blendmode.alpha_over):
+                self.ow_terminal.render()
         self.comp_buffer.render_shaded( self.comp_shader, { "filter_color" : self.sequencer.animated_value("star_fadein") } )
         self.render_floor()
         with blendstate(blendmode.alpha_over):
             self.chamber.render_base( self.sequencer.animated_value("chamber_color") )
+            if(self.sequencer.animated_value("sequence.time") > 8.0):
+                self.ow_player.render()
             self.render_ship()
+
             self.chamber.render_door( self.sequencer.animated_value("door_color"))
-            self.ow_player.render()
 
---
+++ client/applications/gravity/shaders/postfx/station/terminal.glsl
@@ -0,0 +1,38 @@
+#version 330
+uniform sampler2D texBuffer;
+uniform sampler2D modBuffer;
+uniform vec4 filter_color;
+uniform vec2 term_cam;
+uniform float warp;
+uniform float time;
+in vec2 uv;
+
+void main(void) {
+
+    float nrm_x = (0.0 - term_cam.x) - (2.0*(uv.x-0.5));
+    vec2 uv_t = uv;
+    uv_t.x+=((sin(warp*((1-uv.y)*(65*warp))*3.14))*(0.01+warp))*0.02;
+    uv_t.y+=sin( ((nrm_x*nrm_x)*3.14*20)+(time*2.0f))/1024.0;
+
+    uv_t.y*=(0.98)+(0.02-((0.01*(1.0-(nrm_x*nrm_x)))));
+
+
+    float y_idx = uv.y*3.14*20*(1.0+warp);
+    float a_warp = ((1.0+sin(y_idx))/2.0);
+    float b_warp = 0.9+(0.1*(sin(time+uv.x*60*3.14)));
+    float c_warp = 0.9+(0.1*(sin(time+uv.x*90*3.14)));
+    float d_warp = 0.9+(0.1*(sin(time+uv.x*120*3.14)));
+
+    vec4 smpl_base = texture(texBuffer,uv_t) * (1.0-(warp*a_warp)) * vec4(1.0*c_warp,1.0*b_warp,1.0*d_warp,1.0-uv.y);
+    vec4 col = (smpl_base * filter_color)*smpl_base;
+
+    float mod_color_e = 0.9 + ((sin( uv.x*nrm_x+uv.y*23 ) + cos(a_warp*time)) / 120.0);
+    
+    float mod_color = (0.75*mod_color_e)+ (0.25*(1.0-(nrm_x*nrm_x)));
+    if(length(col)>0.5) {
+        gl_FragColor = vec4(1.0,1.0,1.0,1.0) * filter_color * mod_color;
+    } else {
+        gl_FragColor = col + (texture(modBuffer,uv_t)*smpl_base.a) * mod_color;
+    }
+
+}
